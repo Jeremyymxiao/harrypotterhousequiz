9327753b1114b7e4648dbe86ea03fc0c38d8d12d
Jeremyymxiao <xym0645@163.com>
2025-03-25 20:08:30 +0800

    add microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -205,6 +205,15 @@ export default function RootLayout({
             gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || 'G-M4GXJKKP23'}');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "qtqdrfr6pj");
+          `}
+        </Script>
       </head>
       <body className="flex flex-col min-h-screen">
         <LanguageNotification />
